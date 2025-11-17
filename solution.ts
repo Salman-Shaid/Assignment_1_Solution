47b53cdcd62ecbce5b3cbc514c8e987ab02b19ab
Salman-Shaid <salmanshaid9@gmail.com>
2025-11-17 21:17:26 +0600

    first commit

---
+++ solution.ts
@@ -0,0 +1,115 @@
+function formatValue(value: string | number | boolean): string | number | boolean {
+    if(typeof value === "string"){
+        return value.toUpperCase();
+    }else if(typeof value === "number"){
+        return value * 10;
+    }else if (typeof value === "boolean") {
+    return !value;
+  }else{
+    return value;
+  }  
+}
+// console.log(formatValue('hello')); // HELLO
+// console.log(formatValue(5));       // 50
+// console.log(formatValue(true));    // false
+
+
+function getLength(value: string | any[]) : number {
+    if(typeof value === "string") {
+        return value.length;
+    } else if(Array.isArray(value)){
+        return value.length;
+    }else{
+        return 0;
+    }
+}
+
+// console.log(getLength('typescript')); // 10
+// console.log(getLength([10, 20, 30, 40])); // 4
+
+
+class Person{
+    name: string;
+    age : number;
+
+    constructor(name: string, age: number){
+        this.name = name;
+        this.age = age;
+    }
+
+    getDetails(): string {
+        return `Name: ${this.name}, Age: ${this.age}`;
+    }
+}
+
+const person1 = new Person("salman", 30);
+
+// console.log(person1.getDetails());
+
+const person2 = new Person('Alice', 25);
+
+// console.log(person2.getDetails());
+
+
+type Item = {
+    title: string;
+    rating: number;
+};
+
+function filterByRating(items: Item[]): Item[]{
+    return items.filter(item=> item.rating >= 4)
+}
+
+const books = [
+    {title: 'Book A', rating: 4.5},
+    {title: 'Book B', rating: 3.5},
+    {title: 'Book C', rating: 2.5},
+    {title: 'Book D', rating: 5.0},
+    
+];
+
+// console.log(filterByRating(books));
+
+type User = {
+    id: number;
+    name: string;
+    email: string;
+    isActive: boolean;
+};
+
+function filterActiveUsers(users: User[]): User[]{
+    return users.filter(user => user.isActive === true);
+}
+
+const users = [
+    { id: 1, name:'Salman', email: "salman@mail.com", isActive : false},
+    { id: 1, name:'Rakib', email: "rakib@mail.com", isActive : true},
+    { id: 1, name:'Asha', email: "Asha@mail.com", isActive : false},
+    { id: 1, name:'Rumi', email: "rumi@mail.com", isActive : true},
+
+    
+];
+
+// console.log(filterActiveUsers(users));
+
+
+interface Book {
+    title : string;
+    author: string;
+    publishYear: number;
+    isAvailable: boolean;
+}
+
+function printBookDetails(book: Book): void {
+    const availability = book.isAvailable ? "Yes" : "No";
+    console.log(`Title: ${book.title}, Author: ${book.author}, Published: ${book.publishYear}, Available: ${availability}`);
+}
+
+const myBook: Book= {
+    title: "The Great Gatsby",
+    author: "F.Scott Fitzgerald",
+    publishYear: 1925,
+    isAvailable: true,
+};
+
+printBookDetails(myBook);

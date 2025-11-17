0ccd00f76f79e425625bec006947435d74235029
Salman-Shaid <salmanshaid9@gmail.com>
2025-11-17 22:09:39 +0600

    solution number 8 complited

--- solution.ts
+++ solution.ts
@@ -159,13 +159,34 @@ function getUniqueValues(
 
 
 
-type Product = {
+type Products = {
      name : string;
      price : number;
      quantity : number;
      discount?: number;
 };
 
-function calculateTotalPrice(product: Product[]): number {
-    if()
+function calculateTotalPrice(product: Products[]): number {
+    if(product.length === 0 ) {
+        return 0;
+    }
+
+    const total = products.map(p => {
+        const basePrice = p.price*p.quantity;
+        if(p.discount !== undefined){
+            const discountAmount = (basePrice * p.discount) / 100;
+            return basePrice - discountAmount;
+        }
+        return basePrice;
+    }).reduce((sum, value) => sum + value, 0);
+
+    return total;
 }
+
+const products = [
+    {name: 'Pen', price: 10, quantity: 2},
+    {name: 'Notebook', price: 25, quantity: 3, discount: 10},
+    {name: 'Bag', price: 50, quantity: 1, discount: 20},
+];
+
+console.log(calculateTotalPrice(products));

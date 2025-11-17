1dede7dc0b0c6c7c15fe5e31e76e6e9a55d3187c
Salman-Shaid <salmanshaid9@gmail.com>
2025-11-17 21:54:48 +0600

    add solution number 7

--- solution.ts
+++ solution.ts
@@ -111,5 +111,61 @@ const myBook: Book= {
     publishYear: 1925,
     isAvailable: true,
 };
-
 printBookDetails(myBook);
+
+
+
+function getUniqueValues(
+    arr1: (number | string)[],
+    arr2: (number | string) []
+): (number | string)[] {
+
+    const result: (number | string)[] = [];
+
+    for(let i = 0; i < arr1.length; i++){
+        let exists = false;
+
+        for(let j = 0 ; j < result.length; j++){
+            if(result[j] === arr1[i]){
+                exists = true;
+                break;
+            }
+        }
+        if(!exists){
+            result[result.length] = arr1[i]!;
+        }
+    }
+
+    for(let i = 0 ; i < arr2.length; i++) {
+        let exists = false;
+
+        for(let j = 0 ; j< result.length; j++){
+            if(result[j] === arr2[i]) {
+                exists = true;
+                break;
+            }
+        }
+        if(!exists){
+            result[result.length] = arr2[i]!;
+        }
+    }
+    return result;
+}
+
+// const array1 = [1, 2, 3, 4, 5];
+// const array2 = [3, 4, 5, 6, 7]
+
+// console.log(getUniqueValues(array1, array2))
+
+
+
+type Product = {
+     name : string;
+     price : number;
+     quantity : number;
+     discount?: number;
+};
+
+function calculateTotalPrice(product: Product[]): number {
+    if()
+}
